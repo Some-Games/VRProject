169c0ed60278d7b39cf9dc60be2ff0841e40bd37
Chris Christensen <ninjachris098@gmail.com>
2020-04-26 20:34:18 -0700

    Block Spawning, Initialized Move Left/Right/Down

--- VRPuzzle/Assets/Scripts/MainPuzzleLogic.cs
+++ VRPuzzle/Assets/Scripts/MainPuzzleLogic.cs
@@ -1,14 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public enum PuzzleBlockType
 {
-    Block_X_Active,
-    Block_O_Active,
+    Block_X_Active = 0,
+    Block_O_Active = 1,
     Block_X,
     Block_O,
-    Open
+    Open,
+    Closed
+}
+
+public enum PuzzleBlockSize
+{
+    Square,
+    ThreeWide,
+    ThreeTall
 }
 
 public class MainPuzzleLogic : MonoBehaviour
@@ -17,6 +26,11 @@ public class MainPuzzleLogic : MonoBehaviour
     int BoardWidth = 7;
     int BoardHeight = 9;
     List<PuzzleBlockType> PuzzleBlockArray;
+    #endregion
+
+    #region Current Block Information
+    Vector2Int BottomLeftCornerPosition;
+    PuzzleBlockSize BlockSize;
     #endregion
 
     // Start is called before the first frame update
@@ -28,36 +42,9 @@ public class MainPuzzleLogic : MonoBehaviour
     }
 
     // Update is called once per frame
-    float testTimer = 0f;
-    int counterX = 0;
-    int counterY = 0;
     void Update()
     {
-        testTimer += Time.deltaTime;
-        if(testTimer > 0.05f)
-        {
-            testTimer = 0f;
-
-            if (counterX > BoardWidth + 1) // BoardWidth due to +2 width
-            {
-                counterX = 0;
-                ++counterY;
-
-                ClearBoardWalls();
-            }
-
-            if (counterY > BoardHeight - 1) return;
-
-            SetBlockAtBoardPosition(counterX, counterY, PuzzleBlockType.Block_X_Active);
-            
-            ++counterX;
-
-            print("X: " + counterX);
-            print("Y: " + counterY);
-            print("--------------------------------------");
-
-            PrintBoardToConsole();
-        }
+        // UPDATE_TEST();
     }
 
     void Init_PuzzleBlockArray()
@@ -66,12 +53,66 @@ public class MainPuzzleLogic : MonoBehaviour
 
         for (int i = 0; i < (BoardWidth + 2) * BoardHeight; ++i)
             PuzzleBlockArray.Add(PuzzleBlockType.Open);
+
+        SpawnNewBlock();
+    }
+
+    public void SetPuzzleBlockInformation( PuzzleBlockSize blockSize_ )
+    {
+        BlockSize = blockSize_;
+    }
+
+    void SpawnNewBlock()
+    {
+        // TEST
+        // BlockSize = PuzzleBlockSize.ThreeWide;
+
+        // Set X position of next block
+        BottomLeftCornerPosition.x = (BoardWidth / 2);
+        #region Old
+        /*
+        if (BlockSize == PuzzleBlockSize.Square || BlockSize == PuzzleBlockSize.ThreeTall)
+            BottomLeftCornerPosition.x = (BoardWidth / 2);
+        else
+            BottomLeftCornerPosition.x = (BoardWidth / 2) + 1;
+        */
+        #endregion
+
+        // Set Y position of next block
+        if (BlockSize == PuzzleBlockSize.ThreeTall)
+            BottomLeftCornerPosition.y = BoardHeight - 3;
+        else
+            BottomLeftCornerPosition.y = BoardHeight - 2;
+
+        // Populate a new useable block onto the board.
+        // In all scenarios, there will be a 2x2 placed.
+        PuzzleBlockType[] tempArray;
+        if (BlockSize == PuzzleBlockSize.Square) tempArray = new PuzzleBlockType[4]; else tempArray = new PuzzleBlockType[6];
+        for (int i = 0; i < tempArray.Length; ++i)
+            tempArray[i] = (PuzzleBlockType)UnityEngine.Random.Range(0, 2);
+
+        SetBlockAtBoardPosition(BottomLeftCornerPosition.x, BottomLeftCornerPosition.y, tempArray[0]);
+        SetBlockAtBoardPosition(BottomLeftCornerPosition.x + 1, BottomLeftCornerPosition.y, tempArray[1]);
+        SetBlockAtBoardPosition(BottomLeftCornerPosition.x, BottomLeftCornerPosition.y + 1, tempArray[2]);
+        SetBlockAtBoardPosition(BottomLeftCornerPosition.x + 1, BottomLeftCornerPosition.y + 1, tempArray[3]);
+
+        if(BlockSize != PuzzleBlockSize.Square)
+        {
+            if(BlockSize == PuzzleBlockSize.ThreeTall)
+            {
+                SetBlockAtBoardPosition(BottomLeftCornerPosition.x, BottomLeftCornerPosition.y + 2, tempArray[4]);
+                SetBlockAtBoardPosition(BottomLeftCornerPosition.x + 1, BottomLeftCornerPosition.y + 2, tempArray[5]);
+            }
+            else
+            {
+                SetBlockAtBoardPosition(BottomLeftCornerPosition.x + 2, BottomLeftCornerPosition.y, tempArray[4]);
+                SetBlockAtBoardPosition(BottomLeftCornerPosition.x + 2, BottomLeftCornerPosition.y + 1, tempArray[5]);
+            }
+        }
     }
 
     PuzzleBlockType GetBlockAtBoardPosition( int x_, int y_ )
     {
-        // if (x_ == 0 || x_ == BoardWidth + 1) return PuzzleBlockType.Open;
-
         return PuzzleBlockArray[(y_ * (BoardWidth + 2)) + x_];
     }
 
@@ -89,6 +130,23 @@ public class MainPuzzleLogic : MonoBehaviour
         }
     }
 
+    #region Move Blocks
+    void MoveActiveBlocks_Down()
+    {
+        // Find Bottom
+    }
+
+    void MoveActiveBlocks_Left()
+    {
+        // Find Bottom
+    }
+
+    void MoveActiveBlocks_Right()
+    {
+        // Find Bottom
+    }
+    #endregion
+
     void PrintBoardToConsole()
     {
         // Ran in reverse vertically for sake of console printing
@@ -102,6 +160,38 @@ public class MainPuzzleLogic : MonoBehaviour
             }
 
             print( lineString );
+        }
+    }
+
+    float testTimer = 0f;
+    int counterX = 0;
+    int counterY = 0;
+    void UPDATE_TEST()
+    {
+        testTimer += Time.deltaTime;
+        if (testTimer > 0.05f)
+        {
+            testTimer = 0f;
+
+            if (counterX > BoardWidth + 1) // BoardWidth due to +2 width
+            {
+                counterX = 0;
+                ++counterY;
+
+                ClearBoardWalls();
+            }
+
+            if (counterY > BoardHeight - 1) return;
+
+            SetBlockAtBoardPosition(counterX, counterY, PuzzleBlockType.Block_X_Active);
+
+            ++counterX;
+
+            print("X: " + counterX);
+            print("Y: " + counterY);
+            print("--------------------------------------");
+
+            PrintBoardToConsole();
         }
     }
 }
